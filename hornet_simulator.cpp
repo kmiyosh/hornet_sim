b98eff834332b59e174c51ee4db27fa9ce170385
kmiyosh <48099601+kmiyosh@users.noreply.github.com>
2020-12-27 04:45:39 +0900

    Add files via upload
    シミュレーターにコメントを追加

--- hornet_simulator.cpp
+++ hornet_simulator.cpp
@@ -46,29 +46,29 @@ const int datasize = 4;//キャッシュ内のデータは2^4 wordある
 const int associativity = 2;//キャッシュの連想度=2^2
 const int indexsize = 1;//キャッシュのインデックスの数=2^9
 const int cunum = 1;//キャッシュが何個あるか
-const int wavefrontnum = 8;
-const int phasenum = 8;
-const int penum = 8;
-const int max_tree_depth = 384;
-const int wavefrontsize = phasenum * penum;
+const int wavefrontnum = 8;//wavefrontの個数
+const int phasenum = 8;//phaseの個数
+const int penum = 8;//peの個数
+const int max_tree_depth = 384;//木の深さの最大値
+const int wavefrontsize = phasenum * penum;//1つのwavefrontで処理する点の個数
 long long now = 0;//現在のサイクル
-uint32_t pattern[wavefrontnum][max_tree_depth][phasenum][penum];
-int treedepth[wavefrontnum] = { 0 };
-int processingdepth[wavefrontnum] = { 0 };
-bool is_stall[wavefrontnum];
-int accessedaddress[wavefrontnum];
-int frame_count = 0;
-set<int>addresslist;
-int wavefront = 0;
-int phase = 0;
-int arithmetic_count = 0;
-int max_dram_wait = 0;
-int num_cache_out = 0;
-int pe_idle_time = 0;
-int cache_work_time = 0;
-queue<access_t>registerlist;
-set<uint32_t>dramlist;
-bool accessed[wavefrontnum][phasenum][penum];
+uint32_t pattern[wavefrontnum][max_tree_depth][phasenum][penum];//1フレームごとのメモリアクセスパターンを格納する配列
+int treedepth[wavefrontnum] = { 0 };//各wavefrontが処理する木の深さ
+int processingdepth[wavefrontnum] = { 0 };//現在処理している深さをメモしておく配列
+bool is_stall[wavefrontnum];//各wavefrontがストールしているかどうかをメモする配列。trueならストールしている
+int accessedaddress[wavefrontnum];//各wavefront内のphase,peのうち、いくつがほしいアドレスデータを受け取れたかメモする配列。これがwavefrontsizeと等しければ算術演算に移行可能
+int frame_count = 0;//現在処理しているのが何フレーム目かメモする配列
+set<int>addresslist;//キャッシュにリクエストする際、重複を省いたアドレスを格納しておくset
+int wavefront = 0;//現在処理中のwavefront
+int phase = 0;//現在処理中のphase
+int arithmetic_count = 0;//算術演算があと何クロックで終わるかメモする用の変数
+int max_dram_wait = 0;//今回のフレームで、同時のDRAMアクセスは最大何個起こったかメモする変数
+int num_cache_out = 0;//今回のフレームで何回キャッシュアウトが起こったか
+int pe_idle_time = 0;//今回のフレームでPEがニートしていた時間は何クロックあったか
+int cache_work_time = 0;//今回のフレームでキャッシュが動いていた時間は何クロックあったか
+queue<access_t>registerlist;//キャッシュのアクセス待ちを格納
+set<uint32_t>dramlist;//dramのアクセス待ちを格納
+bool accessed[wavefrontnum][phasenum][penum];//各wavefront,phaseにおけるpeがほしいデータを受け取れたか。trueならすでに受け取れている
 typedef struct cacheline {
 	bool valid;//キャッシュの有効bit
 	int tag;//タグ
@@ -90,15 +90,15 @@ void dram_return(access_t access);
 int time_cal(int time) {//timewheelのどこにイベントを登録するか計算
 	return (time) % wheelsize;
 }
-void request_cache(access_t access) {
-	registerlist.push(access);
+void request_cache(access_t access) {//キャッシュがアクセスリクエストを受け取った際の処理
+	registerlist.push(access);//待ち行列にアクセスを追加し
 	if (registerlist.size() == 1) {
-		timewheel[now].push(event{ cache_search, access, 15});
+		timewheel[now].push(event{ cache_search, access, 15});//必要ならキャッシュのhit判定処理を発火
 	}
 }
-void cache_search(access_t access) {//キャッシュのhit判定
-	cache_work_time += 1;
-	access = registerlist.front();
+void cache_search(access_t access) {//キャッシュのhit判定&必要ならDRAMにアクセスを飛ばす
+	cache_work_time += 1;//現在のクロックではキャッシュが働いている
+	access = registerlist.front();//キャッシュの処理待ちキューのtopを見る
 	registerlist.pop();
 	bool hit = false;
 	int cu = 0;//cu番号
@@ -136,12 +136,12 @@ void cache_search(access_t access) {//キャッシュのhit判定
 			}
 			if (cache[cu][index][oldest].valid) { num_cache_out += 1; }
 			cache[cu][index][oldest].valid = false;
-			timewheel[time_cal(now + 205)].push(event{ cache_writeback, access, 5});
-			timewheel[time_cal(now + 205)].push(event{ dram_return, access, 0});
+			timewheel[time_cal(now + 205)].push(event{ cache_writeback, access, 5});//現在の処理で1、dramアクセスで200の合計205クロック後にキャッシュ書き込みが完了
+			timewheel[time_cal(now + 205)].push(event{ dram_return, access, 0});//本当はもうチョイ早く配れるけど同期したほうが実装が楽...
 		}
 	}
 }
-void cache_writeback(access_t access) {
+void cache_writeback(access_t access) {//DRAMからキャッシュへの書き戻し
 	int cu = 0;//cu番号
 	uint32_t address = access.address;//アクセスしたいアドレス
 	uint32_t tag = address >> (indexsize + datasize + 2);//アドレスに対応するタグ
@@ -151,31 +151,31 @@ void cache_writeback(access_t access) {
 	cache[cu][index][rewriteline].tag = tag;
 	cache[cu][index][rewriteline].last_access = now;
 }
-void pe_writeback(access_t access) {
+void pe_writeback(access_t access) {//キャッシュからPEへの書き戻し
 	int wavefront = access.wavefront;
 	int phase = access.phase;
 	for (int pe = 0; pe < penum; pe++) {
-		if (pattern[wavefront][processingdepth[wavefront]][phase][pe] == access.address) {
-			accessed[wavefront][phase][pe] = true;
-			accessedaddress[wavefront] += 1;
+		if (pattern[wavefront][processingdepth[wavefront]][phase][pe] == access.address) {//該当するアドレスを待っていたPEについて
+			accessed[wavefront][phase][pe] = true;//アクセス完了を記録し
+			accessedaddress[wavefront] += 1;//wavefrontのアクセス完了カウントを増やす
 		}
 	}
 }
-void dram_return(access_t access) {
+void dram_return(access_t access) {//dramからPEへの書き戻し
 	uint32_t address = access.address;
-	dramlist.erase(address);
+	dramlist.erase(address);//dramのアクセス待ちを解除
 	for (int wa = 0; wa < wavefrontnum; wa++) {
 		for (int ph = 0; ph < phasenum; ph++) {
 			for (int pe = 0; pe < penum; pe++) {
-				if (accessed[wa][ph][pe]==false&&pattern[wa][processingdepth[wa]][ph][pe] == access.address) {
-					accessed[wa][ph][pe] = true;
-					accessedaddress[wa] += 1;
+				if (accessed[wa][ph][pe]==false&&pattern[wa][processingdepth[wa]][ph][pe] == access.address) {//アクセス待ち状態で、かつ現在帰ってきたアドレスを待っているPEについて
+					accessed[wa][ph][pe] = true;//アクセス完了を記録し
+					accessedaddress[wa] += 1;//wavefrontのアクセス完了カウントを増やす
 				}
 			}
 		}
 	}
 }
-void clear_values() {
+void clear_values() {//プログラムの起動時および各フレームの最後に、必要な変数をクリアする
 	now = 0;
 	hitcount = 0;
 	num_cache_out = 0;
@@ -196,7 +196,7 @@ void clear_values() {
 		}
 	}
 }
-int read_input() {
+int read_input() {//入力を読む関数
 	int wavefront, phase, t, pe;
 	while (cin >>dec>> wavefront >> t >> phase) {
 		treedepth[wavefront] = max(treedepth[wavefront], t);
@@ -214,32 +214,32 @@ int read_input() {
 // キャッシュへのPEからのリクエストの発行
 // キャッシュをしばらくビジーにする
 //
-void memory_read() {
-	addresslist.clear();
+void memory_read() {//PEからキャッシュにアクセスリクエストを飛ばす
+	addresslist.clear();//リクエストするアドレスのsetをいったんクリア
 	for (int pe = 0; pe < penum; pe++) {
-		if (pattern[wavefront][processingdepth[wavefront]][phase][pe] == 0) {
-			accessedaddress[wavefront] += 1;
+		if (pattern[wavefront][processingdepth[wavefront]][phase][pe] == 0) {//もしアドレスが0なら、そのPEは休止中
+			accessedaddress[wavefront] += 1;//すでにアクセスが完了した扱いにしておく
 		}
 		else {
-			addresslist.insert(pattern[wavefront][processingdepth[wavefront]][phase][pe]);
+			addresslist.insert(pattern[wavefront][processingdepth[wavefront]][phase][pe]);//リクエストするアドレスをsetに格納
 		}
 	}
 	for (auto itr = addresslist.begin(); itr != addresslist.end(); itr++) {
-		access_t access = { *itr, wavefront, phase };
+		access_t access = { *itr, wavefront, phase };//相異なるアドレスそれぞれについて、2秒後にキャッシュへリクエストが到着
 		timewheel[time_cal(now + 2)].push(event{ request_cache, access, 0 });
 	}
-	phase += 1;
-	if (phase >= phasenum) {
-		is_stall[wavefront] = true;
+	phase += 1;//一通りリクエストを送ったら次のphaseのLoad命令に移る
+	if (phase >= phasenum) {//最後のphaseまで到着していた場合は、次のwevefrontへ移る
+		is_stall[wavefront] = true;//現在送ったばかりのリクエストは絶対に処理されていないので、当該wavefrontは確実に1度stallする
 		phase = 0;
 		wavefront += 1;
-		if (wavefront >= wavefrontnum) {
+		if (wavefront >= wavefrontnum) {//最後のwavefrontまで到着していた場合は、wavefront0に移る
 			wavefront = 0;
 		}
 	}
 }
-void arithmetic_start() {
-	arithmetic_count = 56;
+void arithmetic_start() {//算術演算の開始
+	arithmetic_count = 55;//算術演算は56クロックかかる。よって現在のクロックを除けば55
 }
 void output_result() {
 	cout << "現在のフレーム: " << frame_count+1<<"\n";
@@ -263,17 +263,13 @@ int main(int argc, char* argv[]) {
 	clear_values();
 	while (read_input() == 0) {//1フレーム分の入力を読み込んで、whileループの中の処理に入る
 		while (1) {
-			if (now==991) {
-				int x;
-				x = 1;
-			}
 			if (registerlist.size() > 0) {
-				timewheel[now].push(event{ cache_search, NULL, 15 });
+				timewheel[time_cal(now)].push(event{ cache_search, NULL, 15 });//もしキャッシュの処理待ちがあれば、hit判定を起動
 			}
 			while (!timewheel[time_cal(now)].empty()) {
-				//キャッシュのイベントを処理
-				event currentevent = timewheel[now].top();
-				timewheel[now].pop();
+				//キャッシュのイベントを順々に処理
+				event currentevent = timewheel[time_cal(now)].top();
+				timewheel[time_cal(now)].pop();
 				access_t access = currentevent.access;
 				auto func = currentevent.callback;
 				func(access);
@@ -298,7 +294,7 @@ int main(int argc, char* argv[]) {
 							}
 						}
 						if (finished) {//もし現在のフレームのすべての計算が終了していたら、次のフレームへ行く
-							output_result();
+							output_result();//フレームの最後でシミュレーション結果を出力
 							frame_count += 1;
 							break;
 						}
